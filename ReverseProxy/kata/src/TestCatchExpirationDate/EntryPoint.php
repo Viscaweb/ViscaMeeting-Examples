42ffd1084fe4d8c6a7bcc0af045eefe995c6a6b9
Jonas HAOUZI <jonas@viscaweb.com>
2016-08-23 10:06:08 +0200

    Kata Reverse Proxy - first exercice (2)

--- ReverseProxy/kata/src/TestCatchExpirationDate/EntryPoint.php
+++ ReverseProxy/kata/src/TestCatchExpirationDate/EntryPoint.php
@@ -1,6 +1,6 @@
 <?php
-//sleep ($sleepingTime = 3); // This pages takes long to run... please, cache it!
-$sleepingTime = 0;
+sleep ($sleepingTime = 3); // This pages takes long to run... please, cache it!
+
 header("Cache-Control: public");
 header("Expires: Tue, 07 Mar 2017 05:00:00 GMT");
 
